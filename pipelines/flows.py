5a1e0249ac435bd486bc1c33c7662fe04c2b45db
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-06-16 19:01:41 -0300

    feat: Flow brt_flow criado e agendada a execução para cada minuto, no arquivo flows.py

---
+++ pipelines/flows.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from prefect import Flow, Parameter
+from prefect.run_configs import LocalRun
+from prefect.storage import Local
+
+from pipelines.tasks import (
+    download_data,
+    parse_data,
+    save_report
+)
+from pipelines.schedules import every_minute_schedule
+# from pipelines import *
+
+
+with Flow("EMD: BRT - Ingerir dados da API BRT") as brt_flow:
+    # Tasks
+    data = download_data()
+    df = parse_data(data)
+    save_report(df)
+
+
+brt_flow.schedule = every_minute_schedule  # atribui o scheduler do flow ao que foi definido no arquivo schedules.py
+brt_flow.storage = Local('./data')
+brt_flow.run_config = LocalRun()
