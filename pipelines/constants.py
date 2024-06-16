27d0e7067505052a040fb9f220d38ecb5fc55bc0
Nicolas Evilasio <nicolas_evilasio@hotmail.com>
2024-06-16 18:57:46 -0300

    feat: classe constants criada em constants.py

---
+++ pipelines/constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class constants(Enum):  # pylint: disable=c0103
+    """
+    Constant values
+    """
+
+    PATH_BASE_DADOS = "/data/brt_gps.csv"
+    DATASET_ID = "emd-desafio-data-eng"
+    TABLE_ID = "brt_dados_gps"
+
+    EMD_DESAFIO_AGENT_LABEL = 'emd-desafio-agent'
